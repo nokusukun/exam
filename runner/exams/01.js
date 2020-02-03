d3bd9b1543eb44cfff3787c5df0c9322e7eee305
24thsaint <rave.arevalo@kingsland.io>
2020-02-03 14:52:07 +0800

    docs(app): Add ReadMe

--- runner/exams/01.js
+++ runner/exams/01.js
@@ -1,1 +1,20 @@
-console.log(process.argv[2], "===", process.argv[3])
+/**
+ * prop:Name
+ * prop:Activity
+ */
+
+function campaignInput(days, bakers, cakes, waffles, pancakes) {
+  let foodMoney = cakes * 45 + waffles * 5.8 + pancakes * 3.2;
+  let grossFoodMoney = foodMoney * bakers * days;
+  let costNumber = grossFoodMoney / 8;
+  let totalRaised = grossFoodMoney - costNumber;
+  console.log(totalRaised.toFixed(2));
+}
+
+const input1 = process.argv[2]
+const input2 = process.argv[3]
+const input3 = process.argv[4]
+const input4 = process.argv[5]
+const input5 = process.argv[6]
+
+campaignInput(input1, input2, input3, input4, input5);
---
+++ runner/tests/01_test.js
@@ -0,0 +1,20 @@
+// Name: Von, Villamor E.
+// Activity: 0-BN-ACT-22
+
+const input1 = process.argv[2]
+const input2 = process.argv[3]
+const input3 = process.argv[4]
+const input4 = process.argv[5]
+const input5 = process.argv[6]
+
+function charityCampaign(days, bakers, cakes, waffles, pancakes) {
+    let profitCakes=cakes*45;
+    let profitWaffles=waffles*5.80;
+    let profitPancakes=pancakes*3.20;
+    let amountPerDay=(profitCakes+profitWaffles+profitPancakes)*bakers;
+    let amountForCampaign=amountPerDay*days;
+    let amountAfterCosts=amountForCampaign*.875;
+    console.log(amountAfterCosts.toFixed(2));
+}
+
+charityCampaign(input1, input2, input3, input4, input5);
